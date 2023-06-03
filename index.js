323646e8d6ba22c8a31a359e67637b7d576253db
Toxinityy <101073828+Toxinityy@users.noreply.github.com>
2023-06-03 21:44:34 +0700

    First Commit
    Public -> Semua file/folders yang public bisa akses
    Views -> Semua file yang akan di render
    Index.js -> main class

---
+++ index.js
@@ -0,0 +1,54 @@
+import mysql from "mysql";
+import path from "path";
+import express from "express";
+import session from "express-session";
+import crypto from "crypto";
+
+const PORT = 8080;
+const app = express();
+
+app.use(
+    session({
+        secret: "my_secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+const pool = mysql.createPool({
+    user: "root",
+    password: "",
+    database: "IDE",
+    host: "localhost",
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const dbConnect = () => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(conn);
+            }
+        });
+    });
+};
+
+app.get("/", async(req,res) => {
+    // const conn = await dbConnect();
+    res.render("home");
+});
+app.get("/login", async(req,res) => {
+    res.render("/login");
+});
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const staticPath = path.resolve("public");
+app.use(express.static(staticPath));
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}!`);
+});
