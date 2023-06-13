1524d758583c8a787a4e7f46d9a922a89aedf060
Toxinityy <101073828+Toxinityy@users.noreply.github.com>
2023-06-13 19:57:35 +0700

    Update with pull request Angel
    Signed-off-by: Toxinityy <101073828+Toxinityy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ import path from "path";
 import express from "express";
 import session from "express-session";
 import crypto from "crypto";
+import memoryStore from 'memorystore';
 
 const PORT = 8080;
 const app = express();
-
+const sessionStore = memoryStore(session);
 app.use(
     session({
+        cookie: {
+            httpOnly: false,
+            sameSite: 'strict',
+            maxAge: 1*60*60*1000
+        },
+        store: new sessionStore({
+            checkPeriod: 1*60*60*1000
+        }),
         secret: "my_secret",
         resave: true,
         saveUninitialized: true,
+        logged_in: false
     })
 );
 const pool = mysql.createPool({
     user: "root",
     password: "",
-    database: "IDE",
+    database: "review_tas",
     host: "localhost",
 });
 
@@ -38,28 +48,189 @@ const dbConnect = () => {
     });
 };
 
+const checkEmail = (conn, email)=>{
+    return new Promise((resolve, reject)=>{
+        conn.query('SELECT * FROM Publik WHERE emailPengguna = ?', [email],(err, result)=>{
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+};
+
+const checkUsername = (conn, username)=>{
+    return new Promise((resolve, reject)=>{
+        conn.query('SELECT * FROM Publik WHERE username = ?', [username],(err, result)=>{
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+};
+
+const insertData = (conn, firstName, lastName, username, email, password)=>{
+    return new Promise((resolve, reject)=>{
+        const date = new Date();
+        conn.query('INSERT INTO Publik (password, username, firstName, lastName, emailPengguna, accountCreatedDate) VALUES (?, ?, ?, ?, ?, ?)', [password, username, firstName, lastName, email, date],(err, result)=>{
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+};
+
 app.get("/", async(req,res) => {
     // const conn = await dbConnect();
     res.render("home");
 });
 app.get("/login", async(req,res) => {
-    res.render("login-public");
+    if(req.session.logged_in){
+        res.redirect('/dashboard-public');
+    }
+    else{
+        res.render("login-public",{
+            emailProblem: '',
+            passwordProblem: '',
+            loginProblem: ''
+        });
+    }
 });
 app.get("/signup", async(req,res) => {
-    res.render("signup");
+    res.render("signup", {
+        emailProblem: '',
+        usernameProblem: '',
+        passwordProblem: '',
+        signupProblem: ''
+    });
+});
+app.get('/adminlogin', async(req, res) => {
+    res.render('login-admin');
 });
 app.get("/dashboard-public", async(req,res) => {
-    res.render("dashboard-public")
+    res.render("dashboard-public",{
+        user: req.session.username
+    })
 });
-app.post("/auth", async (req, res) => {
+app.post("/signup", async (req, res) => {
     // terima nama, email, pass, confirm pass
-    const { name, email, password, confirmpassword } = req.body;
-    
-    // validasi database
+    const{ firstName, lastName, email, username, password, confirmpassword } = req.body;
 
-    // redirect ke dashboard public
-    res.redirect("/dashboard-public");
+    // cek form kosong
+    if(firstName!="" && lastName!="" && email!="" && username!="" && password!= "" && confirmpassword!=""){ 
+        // validasi database
+        const signedUpEmail = await checkEmail(await dbConnect(), email);
+        const signedUpUsername = await checkUsername(await dbConnect(), username);
+
+        // belum ada pengguna dengan email tersebut
+        if(signedUpEmail.length == 0 && signedUpUsername.length == 0){
+            //cek password match
+            if(password == confirmpassword){ // jika match
+                // insert database
+                insertData(await dbConnect(), firstName, lastName, username, email, password).then((result)=>{
+                    // redirect ke dashboard public
+                    res.redirect("/dashboard-public");
+                });
+            }
+            else{ // jika tidak match
+                // pesan kesalahan
+                res.render("signup", {
+                    emailProblem: '',
+                    usernameProblem: '',
+                    passwordProblem: 'Password does not match',
+                    signupProblem: ''
+                });
+            }
+        }
+        else if(signedUpEmail.length != 0 && signedUpUsername.length == 0){ //sudah ada pengguna dengan email tersebut
+            res.render("signup", {
+                emailProblem: 'Email already registered',
+                usernameProblem: '',
+                passwordProblem: '',
+                signupProblem: ''
+            });
+        }
+        else if(signedUpEmail.length == 0 && signedUpUsername.length != 0){ //sudah ada pengguna dengan username tersebut
+            res.render("signup", {
+                emailProblem: '',
+                usernameProblem: 'Username already exists',
+                passwordProblem: '',
+                signupProblem: ''
+            });
+        }
+        else{
+            res.render("signup", {
+                emailProblem: 'Email already registered',
+                usernameProblem: 'Username already exists',
+                passwordProblem: '',
+                signupProblem: ''
+            });
+        }
+    }
+    else{
+        // pesan kesalahan
+        res.render("signup", {
+            emailProblem: '',
+            usernameProblem: '',
+            passwordProblem: '',
+            signupProblem: 'Please insert all the form'
+        });
+    }
 });
+
+app.post("/login", async(req,res)=>{
+    const{ email, password} = req.body;
+    if(email!="" && password!=""){
+        const signedUpEmail = await checkEmail(await dbConnect(), email);
+        if(signedUpEmail.length==0){
+            //email belum terdaftar
+            res.render("login-public",{
+                emailProblem: 'Email does not exists',
+                passwordProblem: '',
+                loginProblem: ''
+            });
+        }
+        else{
+            const registeredPassword = signedUpEmail[0].password;
+            if(registeredPassword == password){
+                //jika pass sesuai maka login berhasil
+                req.session.logged_in = true;
+                req.session.username = signedUpEmail[0].username;
+                res.redirect('/dashboard-public');
+            }
+            else{
+                //jika tidak maka tetap di halaman login
+                res.render("login-public",{
+                    emailProblem: '',
+                    passwordProblem: 'Wrong password',
+                    loginProblem: ''
+                });
+            }
+        }
+    }
+    else{
+        res.render("login-public",{
+            emailProblem: '',
+            passwordProblem: '',
+            loginProblem: 'Please insert all the form'
+        });
+    }
+});
+
+app.get("/account-publik", async(req,res)=>{
+    res.render('account-publik',{
+        user: req.session.username
+    });
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}!`);
 });
