2926b1f3d7fe1a0059027b9b601ea25b59e703a9
Toxinityy <101073828+Toxinityy@users.noreply.github.com>
2023-06-17 15:23:51 +0700

    Search Account & Products Models
    Signed-off-by: Toxinityy <101073828+Toxinityy@users.noreply.github.com>

--- models/productModel.js
+++ models/productModel.js
@@ -1,20 +1,46 @@
 // models/productModel.js
 
-const getProductData = async (conn) => {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM tas';
-        conn.query(query, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            const products = [];
-            for (let row of result) {
-              products.push(row);
-            }
-            resolve(products);
-          }
-        });
+const getProductData = async (conn, searchParams, brand, category) => {
+  return new Promise((resolve, reject) => {
+    let query = 'SELECT * FROM TasDetailed';
+    const params = [];
+
+    if (searchParams) {
+      query += ` WHERE namaTas LIKE '%${searchParams}%'`;
+    }
+
+    if (brand) {
+      if (searchParams) {
+        query += ' AND';
+      } else {
+        query += ' WHERE';
+      }
+      query += ' namaMerk LIKE ?';
+      params.push(`%${brand}%`);
+    }
+
+    if (category) {
+      if (searchParams || brand) {
+        query += ' AND';
+      } else {
+        query += ' WHERE';
+      }
+      query += ' namaKategori LIKE ?';
+      params.push(`%${category}%`);
+    }
+
+    conn.query(query, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        const products = [];
+        for (let row of result) {
+          products.push(row);
+        }
+        resolve(products);
+      }
     });
+  });
 };
 
 export { getProductData };
