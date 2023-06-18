46ba635bbdde76ef1ae07241171d13cf305187c5
angelt123 <135519944+angelt123@users.noreply.github.com>
2023-06-19 00:49:35 +0700

    Add files via upload
    Signed-off-by: angelt123 <135519944+angelt123@users.noreply.github.com>

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ app.get("/login", async(req,res) => {
     if(req.session.logged_in && req.session == 1){
         res.redirect('/dashboard-public');
     }
-    else if(req.session.role == 2){
+    else if(req.session.logged_in && req.session.role == 2){
         res.redirect('/dashboard-admin'); //redirect ke dashboard admin
     }
     else{
@@ -197,7 +197,7 @@ app.get("/signup", async(req,res) => {
     if(req.session.logged_in && req.session.role == 1){
         res.redirect('/dashboard-public');
     }
-    else if(req.session.role == 2){
+    else if(req.session.logged_in && req.session.role == 2){
         res.redirect('/dashboard-admin'); //redirect ke dashboard admin
     }
     else{
@@ -213,7 +213,7 @@ app.get('/adminlogin', async(req, res) => {
     if(req.session.logged_in && req.session.role == 2){
         res.redirect('/dashboard-admin'); //redirect ke dashboard admin
     }
-    else if(req.session.role == 1){
+    else if(req.session.logged_in && req.session.role == 1){
         res.redirect('/dashboard-public');
     }
     else{
