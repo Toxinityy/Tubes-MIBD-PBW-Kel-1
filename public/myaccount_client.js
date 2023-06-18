a99d408fd9adbaa16ea71e2d80f727f19575672f
angelt123 <135519944+angelt123@users.noreply.github.com>
2023-06-18 23:59:44 +0700

    Add files via upload
    Signed-off-by: angelt123 <135519944+angelt123@users.noreply.github.com>

--- public/myaccount_client.js
+++ public/myaccount_client.js
@@ -7,11 +7,24 @@ const timelineContent = document.querySelector("#myaccount-timeline-content");
 const followersContent = document.querySelector("#myaccount-followers-list-box");
 const followingContent = document.querySelector("#myaccount-following-list-box");
 
+const formElement = document.getElementById('myaccount-upload-img-form');
+const imageInputElement = document.getElementById('myaccount-image-input');
+const currentProfileImg = document.getElementById('img-myaccount-profil');
+const buttonCancelUpload = document.getElementById('cancel-img-upload-button');
+
 followersNumber.addEventListener('click', followersClicked);
 followersText.addEventListener('click', followersClicked);
 
 followingNumber.addEventListener('click', followingClicked);
 followingText.addEventListener('click', followingClicked);
+
+function onSuccess(response){
+    return response.json();
+}
+function showResultPost(text){
+    currentProfileImg.src = text.response;
+    formElement.style.visibility = 'hidden';
+}
 
 function followersClicked(){
     timelineContent.style.visibility = 'hidden';
@@ -29,15 +42,36 @@ const listFollowers = document.querySelectorAll('.myaccount-followers-person-box
 const listFollowing = document.querySelectorAll('.myaccount-following-person-box');
 
 listFollowers.forEach(followerUser => {
-    followerUser.addEventListener('click', clickPerson);
+    followerUser.addEventListener('click', ()=>{
+        const idUser = followerUser.id.replace('myaccount-follower','');
+        window.location.href = `/account-publik?id=${idUser}`;
+    });
 });
 
 listFollowing.forEach(followingUser => {
-    followingUser.addEventListener('click', clickPerson);
+    followingUser.addEventListener('click', ()=>{
+        const idUser = followingUser.id.replace('myaccount-following','');
+        window.location.href = `/account-publik?id=${idUser}`;
+    });
 });
 
-function clickPerson(event){
-    console.log('clicked');
-    const idUser = event.target.dataset.value;
-    window.location.href = 'account-publik?id=' + idUser;
-}
+currentProfileImg.addEventListener('click', function() {
+    formElement.style.visibility = "visible"
+});
+
+buttonCancelUpload.addEventListener('click', ()=>{
+    formElement.style.visibility = "hidden";
+})
+
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('image', imageInputElement.files[0]);
+
+    fetch('/my-account', {
+        method: 'POST',
+        body: formData
+    }).then(onSuccess).then(showResultPost);
+});
+
--- public/publicaccount.js
+++ public/publicaccount.js
@@ -7,6 +7,17 @@ const timelineContent = document.querySelector("#account-publik-timeline-content
 const followersContent = document.querySelector("#account-publik-followers-list-box");
 const followingContent = document.querySelector("#account-publik-following-list-box");
 
+const formFollow = document.getElementById('account=publik-form-follow');
+const followButton = document.querySelector('#account-publik-follow-button input[type="submit"]');
+const userId = followButton.id.replace('follow-button',''); 
+
+function onSuccess(response){
+    return response.json();
+}
+function showResultPost(text){
+    console.log(text);
+}
+
 followersNumber.addEventListener('click', followersClicked);
 followersText.addEventListener('click', followersClicked);
 
@@ -29,15 +40,15 @@ const listFollowers = document.querySelectorAll('.account-publik-followers-perso
 const listFollowing = document.querySelectorAll('.account-publik-following-person-box');
 
 listFollowers.forEach(followerUser => {
-    followerUser.addEventListener('click', clickPerson);
+    followerUser.addEventListener('click', ()=>{
+        const idUser = followerUser.id.replace('account-publik-follower','');
+        window.location.href = `/account-publik?id=${idUser}`;
+    });
 });
 
 listFollowing.forEach(followingUser => {
-    followingUser.addEventListener('click', clickPerson);
+    followingUser.addEventListener('click', ()=>{
+        const idUser = followingUser.id.replace('account-publik-following','');
+        window.location.href = `/account-publik?id=${idUser}`;
+    });
 });
-
-function clickPerson(event){
-    console.log('clicked');
-    const idUser = event.target.dataset.value;
-    window.location.href = 'account-publik?id=' + idUser;
-}
